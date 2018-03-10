cb517b21f90fe7240f9c9a9c5acd5d9c54ea55dd
Kjones <kjones404@outlook.com>
2018-03-10 13:30:48 -0600

    Style: update to pep8
    Updated media.py and fresh_tomatoes with comments and formatting.

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -150,6 +150,7 @@ movie_tile_content = '''
 </div>
 '''
 
+
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
--- media.py
+++ media.py
@@ -1,17 +1,23 @@
-import webbrowser
+""" Create Class Movie
 
-# Create Class Movie to store movie title, release date, rating, poster image, and trailer
+Movie holds: movie title, release date, summary, poster image, and trailer.
+
+"""
+
+
 class Movie(object):
-    def __init__(self, movie_title, movie_date, movie_summary, poster_image, trailer_youtube):
-        """ creates a movie object with title, date, rating, poster image, and trailer
+    def __init__(self, movie_title, movie_date, movie_summary, poster_image,
+                 trailer_youtube):
+        """ Creates a movie object
         Args:
             movie_title (str): Holds the title of the movie_title.
-            movie_date (str): Holds the movie release date.
-            movie_rating (str): Holds the movie rating.
+            movie_date (str): Holds the movie release year.
+            movie_summary (str): Holds the movie summary.
             poster_image (str): Holds the url of the poster image.
-            trailer_youtube (str): holds the url of the movie trailer on youtube.com
+            trailer_youtube (str): holds the url of the movie trailer
         Returns:
-            a movie object"""
+            a movie object
+        """
         self.title = movie_title
         self.date = movie_date
         self.summary = movie_summary
